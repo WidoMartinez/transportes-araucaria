4d8ea6bce649fa9ed02561f3edc7ea674ec9afd9
Wido Martinez <widomartinez@gmail.com>
2025-09-05 21:42:53 -0400

    fghfghfg

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+// CORRECCIÓN: Se quitan las llaves {} de la importación
+import sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+	plugins: [
+		react(),
+		tailwindcss(),
+		sitemap({
+			hostname: "https://www.transportesaraucaria.cl",
+			dynamicRoutes: [
+				"/",
+				"/#inicio",
+				"/#servicios",
+				"/#destinos",
+				"/#contacto",
+			],
+			changefreq: "weekly",
+			priority: 0.8,
+		}),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
