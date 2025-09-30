5c2e3bfbead5d1c8baac2f79ec181c76f67d4f35
Wido Martinez <widomartinez@gmail.com>
2025-09-30 12:48:19 -0300

    Fix CORS configuration to allow transportesaraucaria.cl domain

--- backend/server-db.js
+++ backend/server-db.js
@@ -33,7 +33,18 @@ const signParams = (params) => {
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+	origin: [
+		'https://www.transportesaraucaria.cl',
+		'https://transportesaraucaria.cl',
+		'http://localhost:3000',
+		'http://localhost:5173',
+		'http://127.0.0.1:5173'
+	],
+	credentials: true,
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+	allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // --- INICIALIZACIÓN DE BASE DE DATOS ---
 const initializeDatabase = async () => {
